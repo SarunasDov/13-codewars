0cb22b9beb2f8775dbed0673d8af227fb9e7f531
Sarunas <sar.dovydaitis@hotmail.com>
2021-05-16 13:48:32 +0300

    oldest

---
+++ codewars/oppositeNumber.js
@@ -0,0 +1,19 @@
+function differenceInAges(ages) {
+
+    let youngest = Infinity;
+    let oldest = -Infinity;
+
+    for (let i = 0; i < ages.length; i++){
+        if (ages[i] < youngest) {
+            youngest = ages[i];
+        }
+        if (ages[i] > oldest) {
+            oldest = ages[i];
+            
+        }
+    }
+    return youngest, oldest, oldest - youngest;
+}
+
+
+Oldest Youngest uzdavinys
